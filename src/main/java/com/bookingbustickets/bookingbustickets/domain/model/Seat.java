a2d582c3109842810077ea6d07a3f169ededfcd8
dbilic98 <doris.bilic@student.fsre.ba>
2023-10-18 17:16:18 +0200

    Fix data type

--- src/main/java/com/bookingbustickets/bookingbustickets/domain/model/Seat.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/domain/model/Seat.java
@@ -1,9 +1,6 @@
 package com.bookingbustickets.bookingbustickets.domain.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,5 +17,6 @@ public class Seat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Byte seatNumber;
+    @Column(columnDefinition = "TINYINT")
+    private Short seatNumber;
 }
