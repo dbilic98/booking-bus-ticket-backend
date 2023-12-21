5fb4d16153228cde9cfc3d68c76dbc397e7a09d9
dbilic98 <doris.bilic@student.fsre.ba>
2023-12-21 19:06:58 +0100

    Implementation of CRUD operations for the Route entity

--- src/main/java/com/bookingbustickets/bookingbustickets/controller/RouteController.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/RouteController.java
@@ -1,8 +1,11 @@
 package com.bookingbustickets.bookingbustickets.controller;
 
+import com.bookingbustickets.bookingbustickets.controller.request.RequestRouteDto;
+import com.bookingbustickets.bookingbustickets.controller.response.ResponseRouteDto;
+import com.bookingbustickets.bookingbustickets.domain.model.Route;
 import com.bookingbustickets.bookingbustickets.domain.service.RouteService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/routes")
@@ -12,5 +15,31 @@ public class RouteController {
 
     public RouteController(RouteService routeService) {
         this.routeService = routeService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseRouteDto findRouteById(@PathVariable Long id) {
+        Route route = routeService.findRouteById(id);
+        return new ResponseRouteDto(route.getId(), route.getStartPoint(), route.getEndPoint(), route.getBasePrice(), route.getTotalDistance());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseRouteDto createRoute(@RequestBody RequestRouteDto requestRouteDto) {
+        Route createdRoute = routeService.createRoute(requestRouteDto);
+        return new ResponseRouteDto(createdRoute.getId(), createdRoute.getStartPoint(), createdRoute.getEndPoint(), createdRoute.getBasePrice(), createdRoute.getTotalDistance());
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseRouteDto updateRoute(@PathVariable("id") Long id, @RequestBody RequestRouteDto requestRouteDto) {
+        Route updatedRoute = routeService.updateRoute(id, requestRouteDto);
+        return new ResponseRouteDto(updatedRoute.getId(), updatedRoute.getStartPoint(), updatedRoute.getEndPoint(), updatedRoute.getBasePrice(), updatedRoute.getTotalDistance());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRoute(@PathVariable("id") Long id) {
+        routeService.deleteRoute(id);
     }
 }
--- src/main/java/com/bookingbustickets/bookingbustickets/domain/service/RouteService.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/domain/service/RouteService.java
@@ -1,7 +1,12 @@
 package com.bookingbustickets.bookingbustickets.domain.service;
 
+import com.bookingbustickets.bookingbustickets.controller.request.RequestRouteDto;
+import com.bookingbustickets.bookingbustickets.domain.model.Route;
 import com.bookingbustickets.bookingbustickets.domain.repository.RouteRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RouteService {
@@ -10,5 +15,35 @@ public class RouteService {
 
     public RouteService(RouteRepository routeRepository) {
         this.routeRepository = routeRepository;
+    }
+
+    public Route findRouteById(Long id) {
+        Optional<Route> optionalRoute = routeRepository.findById(id);
+        if (optionalRoute.isEmpty()) {
+            throw new RuntimeException("Route with ID" + id + "does not exist");
+        }
+        return optionalRoute.get();
+    }
+
+    public Route createRoute(RequestRouteDto requestRouteDto) {
+        Route createdRoute = new Route(requestRouteDto.getStartPoint(), requestRouteDto.getEndPoint(), requestRouteDto.getBasePrice(), requestRouteDto.getTotalDistance());
+        return routeRepository.save(createdRoute);
+    }
+
+    public Route updateRoute(Long id, RequestRouteDto requestRouteDto) {
+        Route updatedRoute = findRouteById(id);
+        updatedRoute.setStartPoint(requestRouteDto.getStartPoint());
+        updatedRoute.setEndPoint(requestRouteDto.getEndPoint());
+        updatedRoute.setBasePrice(requestRouteDto.getBasePrice());
+        updatedRoute.setTotalDistance(requestRouteDto.getTotalDistance());
+        return routeRepository.save(updatedRoute);
+    }
+
+    public void deleteRoute(Long id) {
+        try {
+            routeRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("Route with ID " + id + "does not exist");
+        }
     }
 }
