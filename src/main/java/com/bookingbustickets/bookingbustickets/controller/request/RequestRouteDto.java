f557a00d73d20b0681634f226921ad145a68f352
dbilic98 <doris.bilic@student.fsre.ba>
2023-12-21 19:03:25 +0100

    Add the request to the route

---
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/request/RequestRouteDto.java
@@ -0,0 +1,21 @@
+package com.bookingbustickets.bookingbustickets.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RequestRouteDto {
+
+    private final String startPoint;
+
+    private final String endPoint;
+
+    private final BigDecimal basePrice;
+
+    private final BigDecimal totalDistance;
+}
