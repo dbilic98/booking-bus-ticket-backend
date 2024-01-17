4ad7e68db3320b246806b1f60c00819c2ac4be96
dbilic98 <doris.bilic@student.fsre.ba>
2024-01-17 13:43:53 +0100

    Refactor URL mapping in PassengerCategoryController

--- src/main/java/com/bookingbustickets/bookingbustickets/controller/PassengerCategoryController.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/PassengerCategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/passenger_categories")
+@RequestMapping("/passenger-categories")
 public class PassengerCategoryController {
 
     private final PassengerCategoryService passengerCategoryService;
