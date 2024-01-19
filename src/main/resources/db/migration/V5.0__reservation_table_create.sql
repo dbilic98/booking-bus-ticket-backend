2ab92eaeed668448d7e6cfbf6b3b5dbe71f2a1b0
dbilic98 <doris.bilic@student.fsre.ba>
2024-01-19 19:07:43 +0100

    Remove passenger_category_id and its foreign key

--- src/main/resources/db/migration/V5.0__reservation_table_create.sql
+++ src/main/resources/db/migration/V5.0__reservation_table_create.sql
@@ -3,7 +3,5 @@ CREATE TABLE reservation
     id                    BIGINT(20) AUTO_INCREMENT NOT NULL,
     date_of_reservation   DATETIME                  NOT NULL,
     status                VARCHAR(20)               NOT NULL,
-    passenger_category_id BIGINT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (passenger_category_id) REFERENCES passenger_category (id)
+    PRIMARY KEY (id)
 );
