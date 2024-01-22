ca2bf53081107f385122f85ec443a97d4912017a
dbilic98 <doris.bilic@student.fsre.ba>
2024-01-22 11:04:30 +0100

    Delete import in ResponseReservationDto

--- src/main/java/com/bookingbustickets/bookingbustickets/controller/response/ResponseReservationDto.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/response/ResponseReservationDto.java
@@ -1,7 +1,6 @@
 package com.bookingbustickets.bookingbustickets.controller.response;
 
 import com.bookingbustickets.bookingbustickets.domain.enumeration.ReservationStatus;
-import com.bookingbustickets.bookingbustickets.domain.model.Reservation;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
