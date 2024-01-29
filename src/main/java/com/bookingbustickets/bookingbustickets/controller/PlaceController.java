05b51dd36ddf992d0c03683f09e755e81e79c409
dbilic98 <doris.bilic@student.fsre.ba>
2024-01-29 17:22:21 +0100

    Implement CRUD operations for the Place entity and update the Route table

---
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/PlaceController.java
@@ -0,0 +1,45 @@
+package com.bookingbustickets.bookingbustickets.controller;
+
+import com.bookingbustickets.bookingbustickets.controller.request.RequestPlaceDto;
+import com.bookingbustickets.bookingbustickets.controller.response.ResponsePlaceDto;
+import com.bookingbustickets.bookingbustickets.domain.model.Place;
+import com.bookingbustickets.bookingbustickets.domain.service.PlaceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+
+    private final PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponsePlaceDto findPlaceById(@PathVariable("id") Long id) {
+        Place place = placeService.findPlaceById(id);
+        return new ResponsePlaceDto(place.getId(), place.getPlaceName());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponsePlaceDto createPlace(@RequestBody RequestPlaceDto requestPlaceDto) {
+        Place createdPlace = placeService.createPlace(requestPlaceDto);
+        return new ResponsePlaceDto(createdPlace.getId(), createdPlace.getPlaceName());
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponsePlaceDto updatePlace(@PathVariable("id") Long id, @RequestBody RequestPlaceDto requestPlaceDto) {
+        Place updatedPlace = placeService.updatePlace(id, requestPlaceDto);
+        return new ResponsePlaceDto(updatedPlace.getId(), updatedPlace.getPlaceName());
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePlace(@PathVariable("id") Long id) {
+        placeService.deletePlace(id);
+    }
+}
