b1060e5aad032d2ac5c7d202513f50730d3724df
dbilic98 <doris.bilic@student.fsre.ba>
2024-01-30 13:25:02 +0100

    Fix the bugs

--- src/main/resources/db/migration/V2.0__route_table_create.sql
+++ src/main/resources/db/migration/V2.0__route_table_create.sql
@@ -4,8 +4,8 @@ CREATE TABLE route
     base_price     FLOAT          NOT NULL,
     total_distance DECIMAL(10, 2) NOT NULL,
     start_place_id BIGINT,
-    end_point_id   BIGINT,
+    end_place_id   BIGINT,
     PRIMARY KEY (id),
     FOREIGN KEY (start_place_id) REFERENCES place (id),
-    FOREIGN KEY (end_point_id) REFERENCES place (id)
+    FOREIGN KEY (end_place_id) REFERENCES place (id)
 );
