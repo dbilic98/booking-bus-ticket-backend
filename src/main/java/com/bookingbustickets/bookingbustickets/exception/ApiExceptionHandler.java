55f1b5819527f91506686efd85295d328bf44242
dbilic98 <doris.bilic@student.fsre.ba>
2024-02-13 19:34:38 +0100

    Update the handleMethodArgumentNotValid method to support error messages.

--- src/main/java/com/bookingbustickets/bookingbustickets/exception/ApiExceptionHandler.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/exception/ApiExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.bookingbustickets.bookingbustickets.exception;
 
 import com.bookingbustickets.bookingbustickets.exception.response.ErrorResponse;
+import lombok.NonNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -22,7 +28,21 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             TicketNotFoundException.class})
 
     public ResponseEntity<Object> handleNotFoundException(Exception e, WebRequest request) {
-        ErrorResponse body = new ErrorResponse(e.getMessage());
+        List<String> reasons = new ArrayList<>();
+        reasons.add(e.getMessage());
+        ErrorResponse body = new ErrorResponse(reasons);
         return handleExceptionInternal(e, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> errors.add(fieldError.getDefaultMessage()));
+        var errorResponse = new ErrorResponse(errors);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
