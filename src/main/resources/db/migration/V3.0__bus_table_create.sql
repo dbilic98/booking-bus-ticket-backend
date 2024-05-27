3b28e1c1c4a7e6c544b40dd83dbdd87c7b8dcd74
dbilic98 <doris.bilic@student.fsre.ba>
2024-05-27 22:00:17 +0200

    Remove seats column from bus table

--- src/main/resources/db/migration/V3.0__bus_table_create.sql
+++ src/main/resources/db/migration/V3.0__bus_table_create.sql
@@ -3,6 +3,5 @@ CREATE TABLE bus
     id            BIGINT(20) AUTO_INCREMENT NOT NULL,
     model         VARCHAR(50)               NOT NULL,
     license_plate VARCHAR(20)               NOT NULL,
-    seats         INTEGER(11)               NOT NULL,
     PRIMARY KEY (id)
 );
