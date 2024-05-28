82b41088a015e127d47421dadf6484c2c14ebeb7
dbilic98 <doris.bilic@student.fsre.ba>
2024-05-28 12:08:10 +0200

    Add Company table and update Bus table with foreign key relationship

---
+++ src/main/resources/db/migration/V3.0__company_table_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE company
+(
+    id           BIGINT(20) AUTO_INCREMENT NOT NULL,
+    company_name VARCHAR(255)              NOT NULL,
+    PRIMARY KEY (id)
+);
--- src/main/resources/db/migration/V4.0__bus_table_create.sql
+++ src/main/resources/db/migration/V4.0__bus_table_create.sql
@@ -3,5 +3,7 @@ CREATE TABLE bus
     id            BIGINT(20) AUTO_INCREMENT NOT NULL,
     model         VARCHAR(50)               NOT NULL,
     license_plate VARCHAR(20)               NOT NULL,
-    PRIMARY KEY (id)
+    company_id    BIGINT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_bus_company_id FOREIGN KEY (company_id) REFERENCES company (id)
 );
