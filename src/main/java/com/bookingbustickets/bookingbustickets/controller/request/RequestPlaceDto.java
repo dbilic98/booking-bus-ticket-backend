92c76db6522704408d3b6bac899692f3984f4adc
dbilic98 <doris.bilic@student.fsre.ba>
2024-05-31 11:50:04 +0200

    Convert RequestPlaceDto from class to record

--- src/main/java/com/bookingbustickets/bookingbustickets/controller/request/RequestPlaceDto.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/request/RequestPlaceDto.java
@@ -2,16 +2,9 @@ package com.bookingbustickets.bookingbustickets.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class RequestPlaceDto {
-
-    @NotBlank(message = "Place name is mandatory")
-    @Size(min = 4, max = 50, message = "Place name should be at least 4 characters, and should not exceed 50 characters.")
-    private final String placeName;
+public record RequestPlaceDto(
+        @NotBlank(message = "Place name is mandatory")
+        @Size(min = 4, max = 50, message = "Place name should be at least 4 characters, and should not exceed 50 characters.")
+        String placeName) {
 }
