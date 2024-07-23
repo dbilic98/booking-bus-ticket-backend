6f02c5631fd2d6d7e77f323b664e3348bd486a44
dbilic98 <doris.bilic@student.fsre.ba>
2024-07-23 13:29:02 +0200

    Refactor Lombok imports to use wildcard import

--- src/main/java/com/bookingbustickets/bookingbustickets/domain/model/Ticket.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/domain/model/Ticket.java
@@ -1,10 +1,7 @@
 package com.bookingbustickets.bookingbustickets.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
