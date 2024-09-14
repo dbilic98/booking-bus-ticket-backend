c3e1f412839a8c835020ea83b539512c93d47aaf
dbilic98 <doris.bilic@student.fsre.ba>
2024-09-14 22:08:08 +0200

    Refactored RequestScheduleDto to Java record

--- src/main/java/com/bookingbustickets/bookingbustickets/controller/request/RequestScheduleDto.java
+++ src/main/java/com/bookingbustickets/bookingbustickets/controller/request/RequestScheduleDto.java
@@ -2,29 +2,14 @@ package com.bookingbustickets.bookingbustickets.controller.request;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class RequestScheduleDto {
 
-    @NotNull(message = "Schedule date is mandatory")
-    @FutureOrPresent(message = "The date must be today or in the future")
-    private final LocalDate scheduleDate;
+public record RequestScheduleDto(
+        @NotNull(message = "Schedule date is mandatory") @FutureOrPresent(message = "The date must be today or in the future") LocalDate scheduleDate,
+        @NotNull(message = "Departure time is mandatory") LocalTime departureTime,
+        @NotNull(message = "Arrival time is mandatory") LocalTime arrivalTime, Long routeId, Long busId) {
 
-    @NotNull(message = "Departure time is mandatory")
-    private final LocalTime departureTime;
-
-    @NotNull(message = "Arrival time is mandatory")
-    private final LocalTime arrivalTime;
-
-    private final Long routeId;
-
-    private final Long busId;
 }
